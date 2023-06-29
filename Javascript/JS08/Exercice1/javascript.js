013e515520f3285735f96678a5ef720c6bf05d4c
erodonx <stalcha01@gmail.com>
2023-06-29 16:54:06 +0200

    Prenom

--- Javascript/JS08/Exercice1/javascript.js
+++ Javascript/JS08/Exercice1/javascript.js
@@ -6,10 +6,15 @@ while (isNaN(prenom)==false)
 {
     prenom=window.prompt("Saisir a nouveau le prénom");
 }
+nombre=1;
 while ((prenom.length)!=0){
     tableau.push(prenom);
     prenom=window.prompt(nombre);
     nombre=+nombre+1;
+    while (isNaN(prenom)==false)
+    {
+        prenom=window.prompt("Saisir a nouveau le "+nombre+" prénom");
+    } 
 }
 for (var i in tableau) 
 {
