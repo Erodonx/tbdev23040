51c9645d09b7717fa3784ac6948a0590a57c622c
erodonx <stalcha01@gmail.com>
2023-06-29 16:45:52 +0200

    Chachacha

---
+++ Javascript/JS08/Exercice1/javascript.js
@@ -0,0 +1,19 @@
+window.alert("On va utiliser un tableau pour stocker les prénoms");
+var nombre=0;
+var tableau=[];
+var prenom=window.prompt("Saisissez un prénom");
+while (isNaN(prenom)==false)
+{
+    prenom=window.prompt("Saisir a nouveau le prénom");
+}
+while ((prenom.length)!=0){
+    tableau.push(prenom);
+    prenom=window.prompt(nombre);
+    nombre=+nombre+1;
+}
+for (var i in tableau) 
+{
+ console.log(tableau[i]);
+}
+
+
